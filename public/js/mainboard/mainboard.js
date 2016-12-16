dd80c27c28b2afda71ab15d802ba2a1e0711666e
lipeng <158791268@qq.com>
2016-12-16 16:37:56 +0800

    data - temp

--- public/js/mainboard/mainboard.js
+++ public/js/mainboard/mainboard.js
@@ -18,12 +18,35 @@
     var app = angular.module('dafifo.mainboard', []);
 
     app.controller('dafifo.mainboard.mainboardController', ['$scope','$rootScope','dafifo.mainboard.mainboardFactory',function($scope,$rootScope,fac){
+        fac.getMenu(function(data){
+            $scope.nodeArray = data;
+        });
 
+        fac.getApps(function(data){
+            $scope.appData = data;
+        });
+
+        $scope.nodeHoverEvent = function(parentNode){
+            $scope.childNodeArray = parentNode.children;
+        };
     }]);
 
     app.factory('dafifo.mainboard.mainboardFactory', ['$http',function($http){
         return {
-
+            getMenu : function( callback ){
+                $http.get('/json/menu-data.json').success(function(data, header, config, status){
+                    if(data){
+                        return callback && callback(data);
+                    }
+                });
+            },
+            getApps : function( callback ){
+                 $http.get('/json/app-data.json').success(function(data, header, config, status){
+                      if(data){
+                          return callback && callback(data);
+                      }
+                 });
+            }
         };
     }]);
 
